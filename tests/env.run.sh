8742d3f873dec1e47553b7c2f51665668d2a4d81
Jérôme Blanchard <jblanchard@jahia.com>
2025-03-04 14:15:48 +0100

    Use env to provision template module

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -81,7 +81,7 @@ if compgen -G "./artifacts/*-SNAPSHOT.jar" > /dev/null; then
     echo "$(date +'%d %B %Y - %k:%M') == Content of the artifacts/ folder"
     ls -lah
     echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Will start submitting files"
-    for file in $(ls -1 org.jahia.bundles.client-cache-control-*-SNAPSHOT.jar | sort -n)
+    for file in $(ls -1 *-SNAPSHOT.jar | sort -n)
     do
       echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Submitting module from: $file =="
       curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script='[{"installAndStartBundle":"'"$file"'", "forceUpdate":true}]' --form file=@$file
