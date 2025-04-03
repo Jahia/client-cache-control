03381d3a3b50653114af553b82aa308ef05613c4
Jérôme Blanchard <jblanchard@jahia.com>
2025-04-03 15:16:06 +0200

    Fix nightly test

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -80,10 +80,10 @@ for file in $(ls -1 *-SNAPSHOT.jar | sort -n)
 do
   echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Submitting module from: $file =="
   curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script='[{"installAndStartBundle":"'"$file"'", "forceUpdate":true}]' --form file=@$file
+  sleep 5;
   echo
   echo "$(date +'%d %B %Y - %k:%M') [MODULE_INSTALL] == Module submitted =="
 done
-
 
 echo "$(date +'%d %B %Y - %k:%M') == Executing manifest: ${MANIFEST} =="
 curl -u root:${SUPER_USER_PASSWORD} -X POST ${JAHIA_URL}/modules/api/provisioning --form script="@./run-artifacts/${MANIFEST};type=text/yaml" $(find assets -type f | sed -E 's/^(.+)$/--form file=\"@\1\"/' | xargs)
