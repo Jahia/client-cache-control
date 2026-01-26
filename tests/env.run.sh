3919e0d58698d1eee78d7e6ca7d333e0dd07ba02
Jérôme Blanchard <jblanchard@jahia.com>
2026-01-26 16:19:36 +0100

    chore: fix the feature version to be provisioned on the Jahia release to 9.0.1

--- tests/env.run.sh
+++ tests/env.run.sh
@@ -66,10 +66,7 @@ if compgen -G "./artifacts/org.jahia.bundles.client-cache-control-*-SNAPSHOT.jar
     echo "Uninstalling client-cache-control from others nodes if cluster is enabled"
     if [[ "${JAHIA_CLUSTER_ENABLED}" == "true" ]]; then
         curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-a:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-impl"}]'
-        curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-b:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-impl"}]'
         curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-a:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-api"}]'
-        curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-b:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-api"}]'
-        curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-a:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-graphql"}]'
         curl -u root:${SUPER_USER_PASSWORD} -X POST http://jahia-browsing-b:8080/modules/api/provisioning --form script='[{"uninstallBundle":"org.jahia.bundles.client-cache-control-graphql"}]'
     fi
 fi
